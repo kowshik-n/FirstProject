8ebc2ccec47700b0e3a06073cb806e6dac2e8e07
kowshik-n <79784036+kowshik-n@users.noreply.github.com>
2021-10-07 20:45:16 +0530

    changed factorial num to 10

--- Factorial.java
+++ Factorial.java
@@ -5,7 +5,7 @@ public class Factorial {
     public static void main(String[] args) {
 
         //5! = 5 * (4 * (3 * (2 * (1) = 120
-        System.out.println(factorial(5));
+        System.out.println(factorial(10));
     }
 
     public static int factorial(int n) {
